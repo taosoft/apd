4dd3ad2c1c62902efc54715b2b38412568e60053
Nico-Santos <santos.m.nicolas@gmail.com>
2021-09-22 13:17:31 -0300

    Screens Creadas

---
+++ screens/ComercioInicio.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, Text, Alert, TouchableOpacity } from 'react-native';
+
+export default function Denuncia({ navigation }) {
+
+  return (
+    <View style={styles.container}>
+        
+        
+        
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
