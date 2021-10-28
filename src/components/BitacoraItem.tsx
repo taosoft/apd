957983216aa756b14bc2b97f713bc65a88ddd119
Buzzetti Martin <64173765+buzzettimartin@users.noreply.github.com>
2021-10-28 19:27:19 -0300

    (WIP) Creacion de comercioListado

---
+++ src/components/BitacoraItem.tsx
@@ -0,0 +1,62 @@
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { Button, Card } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+export interface BitacoraItemProps {
+  titulo: string
+  fecha: string
+  foto: string
+}
+
+export default function BitacoraItem(props: BitacoraItemProps): JSX.Element {
+  const navigation = useNavigation()
+
+  return (
+    <View style={styles.container}>
+      <Card containerStyle={{ width: Dimensions.get('window').width - 30 }}>
+        <Card.Title>{props.titulo}</Card.Title>
+        <Card.Divider />
+        <Card.Image
+          source={require('../../borrar-ImagenComercio.jpg')}
+          style={styles.foto}
+        />
+        <Button
+          icon={<Icon color="white" name="plus" size={20} />}
+          onPress={() => {
+            navigation.navigate('ComercioDetalle')
+          }}
+          style={styles.cardButton}
+          title=" Ver más"
+        />
+        <Text style={styles.fecha}>Fecha de publicación: {props.fecha}</Text>
+      </Card>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  cardButton: {
+    borderRadius: 0,
+    bottom: 0,
+    marginBottom: 10,
+    marginLeft: 0,
+    marginRight: 0,
+    position: 'relative',
+  },
+  container: {
+    alignItems: 'flex-end',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  fecha: {
+    color: '#000',
+    fontWeight: 'normal',
+    textAlign: 'left',
+  },
+  foto: {
+    width: '100%',
+  },
+})
