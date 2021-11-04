f140ec56cb6c790b2d91c2cd024280d943c46dd4
Thomas B <braithwaite.th@gmail.com>
2021-11-04 17:19:51 -0300

    Change logo

--- src/screens/Inicio.tsx
+++ src/screens/Inicio.tsx
@@ -20,7 +20,7 @@ export default function Inicio(): JSX.Element {
   return (
     <View style={styles().container}>
       <Image
-        source={{ uri: 'https://res.cloudinary.com/dmdkxer66/image/upload/v1636056926/Logos/MunicipaliApp_1_xv6gcq.png' }}
+        source={{ uri: 'https://res.cloudinary.com/dmdkxer66/image/upload/v1636057175/Logos/MunicipaliApp_2_qa5xdv.png' }}
         style={{ height: windowHeight * 0.8, width: windowWidth }}
       />
 
