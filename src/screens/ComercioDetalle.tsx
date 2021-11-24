1237b05e9055be8f0128d9086a44ac192e587610
Thomas B <braithwaite.th@gmail.com>
2021-11-24 00:40:33 -0300

    Comercio Images to backend

--- src/screens/ComercioDetalle.tsx
+++ src/screens/ComercioDetalle.tsx
@@ -17,6 +17,11 @@ export default function ComercioDetalle({
   const { getComercioDetalle } = useComercio()
   const [comercio, setComercio] = useState<ComercioModel>()
 
+  const imagenes =
+  comercio?.archivosURL?.split(';').map((image) => {
+    return { uri: image }
+  }) ?? []
+
   useEffect(() => {
     getComercioDetalle(id).then((res) => {
       setComercio(res)
@@ -36,59 +41,11 @@ export default function ComercioDetalle({
           <Text style={styles.textSubBold}>Descripcion</Text>
           <Text style={styles.datosDescripcion}>{comercio?.descripcion}</Text>
           <Text style={styles.textSubBold}>Imagenes</Text>
-          <ImageLayout
-            images={[
-              // Version *3.0.0 update (or greater versions):
-              // Can be used with different image object fieldnames.
-              // Ex. source, source.uri, uri, URI, url, URL
-              {
-                uri: 'https://www.olavarria.gov.ar/wp-content/uploads/2017/12/Plaza-Julio-Pagano-B%C2%BA-Bancario-1-1024x683.jpg',
-              },
-              // IMPORTANT: It is REQUIRED for LOCAL IMAGES
-              // to include a dimensions field with the
-              // actual width and height of the image or
-              // it will throw an error.
-              // { source: require("yourApp/image.png"),
-              //     dimensions: { width: 1080, height: 1920 }
-              // },
-              // "width" & "height" is an alternative to the dimensions
-              // field that will also be acceptable.
-              // { source: require("yourApp/image.png"),
-              //     width: 1080,
-              //     height: 1920 },
-              {
-                source: {
-                  uri: 'https://www.pergamino.gob.ar/wp-content/uploads/2019/09/juegos-Plaza-Da%CC%81vila-7-1024x682.jpeg',
-                },
-              },
-              {
-                // Optional: Adding a dimensions field with
-                // the actual width and height for REMOTE IMAGES
-                // will help improve performance.
-                dimensions: { height: 1920, width: 1080 },
-
-                uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Plaza_25_de_Mayo_Rosario_2.jpg/1200px-Plaza_25_de_Mayo_Rosario_2.jpg',
-              },
-              {
-                URI: 'https://www.msm.gov.ar/wp-content/uploads/2014/12/PLAZA-TRUJUI.jpg',
-                // Version *2.0.0 update (or greater versions):
-                // Optional: Does not require an id for each
-                // image object, but is for best practices and
-                // can be better for performance with the API.
-                id: 'blpccx4cn',
-              },
-              {
-                url: 'https://greatruns.com/wp-content/uploads/2017/05/plaza-holanda.jpg',
-              },
-              {
-                URL: 'https://www.welcomeargentina.com/paseos/plazas_mendoza/plazas_mendoza-2.jpg',
-              },
-            ]}
-            // Version *5.7.0 update
-            // onEndReached={() => {
-            //     // add more images when scroll reaches end
-            // }}
-          />
+          {imagenes.length === 0 && (
+            <Text style={styles.alert}>No hay imágenes disponibles</Text>
+          )}
+          {imagenes.length !== 0 && <ImageLayout images={imagenes} />}
+          
         </View>
       </ScrollView>
     </View>
